282c2498bec1926785380f160b8c1fbcf913b7ac
Muhamamd Irfan Pratama <kakakepankuu@gmail.com>
2022-01-24 19:06:28 +0700

    Tugas JS ke-27
    tugas js ke 27 niomic

---
+++ Tugas_JS27.js
@@ -0,0 +1,14 @@
+function js27() {
+    let a = "No";
+    let b = " rumah saya ";
+    let c = 30;
+    let d = "A";
+    let kalimat = a + b + String(c) + d;
+    console.log(kalimat);
+
+    let e = "Jalan pangeran Tirtayasa No ";
+    let f = 70;
+    let kalimat2 = e + String(f);
+    console.log(kalimat2);
+}
+js27();
